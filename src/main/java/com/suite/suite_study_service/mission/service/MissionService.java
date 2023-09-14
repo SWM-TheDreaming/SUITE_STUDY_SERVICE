351344d0aaec18ba47d4d144a07a14dc7670f48d
반영환 <lopahn2@gmail.com>
2023-09-14 19:48:46 +0900

    feat: [스터디 그룹 미션 목록][S] 프로덕트 코드 작성

--- src/main/java/com/suite/suite_study_service/mission/service/MissionService.java
+++ src/main/java/com/suite/suite_study_service/mission/service/MissionService.java
@@ -1,7 +1,12 @@
 package com.suite.suite_study_service.mission.service;
 
 import com.suite.suite_study_service.mission.dto.ReqMissionDto;
+import com.suite.suite_study_service.mission.entity.Mission;
+
+import java.util.List;
 
 public interface MissionService {
     void createMission(ReqMissionDto reqMissionDto);
+
+    List<Mission> listUpMission(Long suiteRoomId, String missionTypeString);
 }
